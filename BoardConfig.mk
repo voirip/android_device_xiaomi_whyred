e06ec9f81ff403220572689060a12dc1a4a68354
marchvps <marchvps2020@gmail.com>
2026-02-15 14:25:40 +0000

    tmp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-11-01
+VENDOR_SECURITY_PATCH := 2026-02-01
 
 # Inherit the proprietary files
 include vendor/xiaomi/whyred/BoardConfigVendor.mk
